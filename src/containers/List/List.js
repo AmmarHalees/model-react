0619325dc81565a624965cf043d8de6d443d1125
Ammar Halees <haleesammar@gmail.com>
2020-06-27 17:54:33 +0300

    using json placeholder

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -7,9 +7,9 @@ class List extends Component {
     }
 
     componentDidMount() {
-
-        fetch('http://localhost:3004/posts')
-        .then(response=>response.json())
+        
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
         .then(jsonData => this.setState({
 
             data:jsonData
