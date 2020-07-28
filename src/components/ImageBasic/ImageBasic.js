645961d2c8af2fe926b78fc01996bdb16db6967f
AmmarHal <haleesammar@gmail.com>
2020-07-28 14:13:57 +0300

    added image types and fixed helper method bug for modal

--- src/components/ImageBasic/ImageBasic.js
+++ src/components/ImageBasic/ImageBasic.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './ImageBasic.module.css';
-import data from '../../utils/constants/strings.json'
+import data from '../../utils/constants/strings.json';
+import { mapImageTypeToClass } from '../../utils/helpers';
 
 
-const ImageBasic = ({src = data.defaultsrc, alt=""}) => {
-    return (<img className={style.image} src ={src} alt={alt}/>  );
+const ImageBasic = ({src = data.defaultsrc, alt="",type}) => {
+    return (<img className={`${style.image} ${style[mapImageTypeToClass[type]]}`} src ={src} alt={alt}/>  );
 }
  
 export default ImageBasic;
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,9 @@
 export const mapTypeToButtonClass = JSON.parse('{"primary":"primary", "secondary":"secondary",  "terinary":"terinary",  "link":"link"}')
 
+export const mapImageTypeToClass = JSON.parse('{"modal image":"modalImage"}')
 
 export const doSomethingAsync =(yourCallbackFcn, before, after , duration) =>{ console.log('click'); yourCallbackFcn(before); window.setTimeout( ()=>{ yourCallbackFcn(after) } , duration ) };
 
 export const isDevelopment =() => process.env.NODE_ENV === 'development';
 
-export const getCSSvariableValue = (variableName) => getComputedStyle(document.documentElement).getPropertyValue('--bg-accent-color');
+export const getCSSvariableValue = (variableName) => getComputedStyle(document.documentElement).getPropertyValue(variableName);
