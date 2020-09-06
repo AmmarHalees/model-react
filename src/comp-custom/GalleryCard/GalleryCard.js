abdad77f1acf2f1736521de4885626919007164b
Ammar Halees <haleesammar@gmail.com>
2020-09-07 01:42:08 +0300

    working on post card

---
+++ src/comp-custom/GalleryCard/GalleryCard.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Image } from 'gestalt';
+import styles from './GalleryCard.module.css';
+import ButtonIcon from '../../components/ButtonIcon/ButtonIcon';
+import { ReactComponent as CloseIcon } from '../../assets/icons/x.svg';
+import { ReactComponent as HeartIcon } from '../../assets/icons/heart.svg';
+import { ReactComponent as TagIcon } from '../../assets/icons/share.svg';
+import ImageBasic from '../../components/ImageBasic/ImageBasic';
+
+const GalleryCard = ({ data, location, onControlClick }) => {
+    return (
+
+
+        <Link
+            push
+            key={data.id}
+            to={{
+                pathname: `/photo/${data.id}`,
+                state: { background: location }
+            }}
+        >
+            <div className={styles.postCard}>
+
+                <Image
+                    alt={data.alt_description}
+                    color={data.color}
+                    naturalHeight={data.height}
+                    naturalWidth={data.width}
+                    src={data.urls.small}
+
+                    // srcSet = {`${data.urls.small} 400w,${data.urls.regular} 800w, ${data.urls.full} 1000w`}
+                    loading='lazy'
+                />
+
+                <div className={styles.postControls}>
+
+                    <h2 className={styles.title}>{data.alt_description}</h2>
+
+                    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', alignItems: 'center' }}>
+
+                        <Link to={`/@${data.user.id}`}>
+                            <ImageBasic rounded type='post' src={data.user.profile_image.medium} />
+
+                        </Link>
+
+                        <div className={styles.postControlButtons}>
+
+                            <ButtonIcon icon={<CloseIcon />} size='medium' title='Close' type='link'
+                                onClick={(e) => onControlClick(e, data.id, 'close')}
+                            />
+                            <ButtonIcon icon={<HeartIcon />} size='medium' title='Like' type='link'
+                                onClick={(e) => onControlClick(e, data.id, 'heart')}
+                            />
+                            <ButtonIcon icon={<TagIcon />} size='medium' title='Save' type='link'
+                                onClick={(e) => onControlClick(e, data.id, 'tag')}
+                            />
+
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </Link>
+
+
+    );
+}
+
+export default GalleryCard;
--- src/components/Galleries/MansoryGallery/MansoryGallery.js
+++ src/components/Galleries/MansoryGallery/MansoryGallery.js
@@ -2,40 +2,33 @@ import React from 'react';
 import { Masonry, Image } from 'gestalt';
 import Spinner from '../../Loading/Spinner/Spinner';
 import { Link, useLocation } from 'react-router-dom';
+import GalleryCard from '../../../comp-custom/GalleryCard/GalleryCard';
 
 const MansoryGallery = ({ data, loadMore , loading , error }) => {
 
 
     let location = useLocation();
 
-    return (
 
+    function onControlClick(e, id, type) {
+
+        e.stopPropagation();
+        e.preventDefault();
+
+        console.log(id)
+
+    }
+
+
+    return (
 
         <>
             <Masonry
                 flexible
                 comp={({ data }) => (
-                    <Link
-                    push
-                      key={data.id}
-                      to={{
-                          pathname: `/photo/${data.id}`,
-                          state: { background: location }
-                      }}
-                    >                        <Image
-                            alt={data.alt_description}
-                            color={data.color}
-                            naturalHeight={data.height}
-                            naturalWidth={data.width}
-                            src={data.urls.small}
-
-                            // srcSet = {`${data.urls.small} 400w,${data.urls.regular} 800w, ${data.urls.full} 1000w`}
-                            loading='lazy'
-                        />
-                        <p>{data.name}</p>
-                    </Link>
+                    <GalleryCard location={location} data={data} onControlClick={onControlClick}/>
                 )}
-                gutterWidth={5}
+                gutterWidth={20}
                 columnWidth = {300}
                 items={data}
                 minCols={1}
